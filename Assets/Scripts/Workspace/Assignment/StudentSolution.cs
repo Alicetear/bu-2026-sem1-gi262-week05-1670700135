fc9314c1b5c1255657bd67aa3a0e08ed80e82ac6
Alicetear <phrnakanth@gmail.com>
2026-09-15 19:22:46 +0700

    Assignment finish

--- Assets/Scripts/Workspace/Assignment/StudentSolution.cs
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
+using System;
 
 namespace Assignment
 {
@@ -69,22 +70,98 @@ namespace Assignment
 
         public int[] AS01_SelectionSortDescending(int[] numbers)
         {
+            int n = numbers.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                int maxIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (numbers[j] > numbers[maxIndex])
+                    {
+                        maxIndex = j;
+                    }
+                }
+
+                int temp = numbers[i];
+                numbers[i] = numbers[maxIndex];
+                numbers[maxIndex] = temp;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
             return numbers;
         }
 
         public int[] AS02_BubbleSortDescending(int[] numbers)
         {
+            int n = numbers.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - 1 - i; j++)
+                {
+                    if (numbers[j] < numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
             return numbers;
         }
 
         public int[] AS03_InsertionSortDescending(int[] numbers)
         {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int key = numbers[i];
+                int j = i - 1;
+
+                while (j >= 0 && numbers[j] < key)
+                {
+                    numbers[j + 1] = numbers[j];
+                    j--;
+                }
+
+                numbers[j + 1] = key;
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
             return numbers;
         }
 
         public int AS04_FindTheSecondLargestNumber(int[] numbers)
         {
-            return 0;
+            Array.Sort(numbers);
+            Array.Reverse(numbers);
+
+            int largest = numbers[0];
+            int secondLargest = 0;
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < largest)
+                {
+                    secondLargest = numbers[i];
+                    break;
+                }
+            }
+
+            Console.WriteLine(secondLargest);
+
+            return secondLargest;
         }
 
         #endregion
@@ -93,7 +170,41 @@ namespace Assignment
 
         public int EX01_FindLongestConsecutiveSequence(int[] numbers)
         {
-            return 0;
+            if (numbers == null || numbers.Length == 0)
+            {
+                Console.WriteLine("The longest consecutive sequence is: 0");
+                return 0;
+            }
+
+            Array.Sort(numbers);
+
+            int maxStreak = 1;
+            int currentStreak = 1;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    continue;
+                }
+
+                if (numbers[i + 1] == numbers[i] + 1)
+                {
+                    currentStreak++;
+                }
+                else
+                {
+                    currentStreak = 1;
+                }
+
+                if (currentStreak > maxStreak)
+                {
+                    maxStreak = currentStreak;
+                }
+            }
+
+            Console.WriteLine($"The longest consecutive sequence is: {maxStreak}");
+            return maxStreak;
         }
 
         #endregion
